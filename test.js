8588740d14ac1599744c0c3565535c14039f7ae9
Djaafar Boumaza <26055564+djaafarcode@users.noreply.github.com>
2017-08-22 12:27:39 +0200

    add cours

--- test.js
+++ test.js
@@ -12,3 +12,15 @@ fetch("https://dog.ceo/api/breeds/list/allsrgrer") //test mauvaise url pour voir
     console.log(err);
     process.exit(1);
 })
+/*
+Pour tester : npm test
+Pour tester côter serveur : node test.js
+
+########## AJOUTER UNE VERSION PATH ##########
+1. Faire une commit.
+2. Voir sur quel version nous sommes : npm version.
+3. Monter d'une version : npm version <patch/minor/major).
+4. Envoyer la version : git push --tags.
+5. Publier la nouvelle version : npm publish.
+
+*/
